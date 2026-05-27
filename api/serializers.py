67fb28f12edd7c6323dd89fc5ed18beee1d935a6
AkshaiKumar <akshayasok47@gmail.com>
2026-05-27 17:41:34 +0530

    Add rest API's

--- api/serializers.py
+++ api/serializers.py
@@ -3,10 +3,11 @@ from rest_framework import serializers
 
 
 class UserSerializer(serializers.ModelSerializer):
-  class meta:
+  class Meta:
     model = User
     fields =  ["id", "username", "password"]
-    extra_kwargs = {"password": {"Write_only: True"} }
+    extra_kwargs = {"password": {"write_only" : True}}
+    
   def create(self, validated_data):
     user = User.objects.create_user(**validated_data)
     return user
